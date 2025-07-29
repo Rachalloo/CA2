5dd8bcc0b5676429dccc5216e28167134eeb2d87
Rachalloo <24009746@myrp.edu.sg>
2025-07-29 21:58:50 +0800

    update

--- app.js
+++ app.js
@@ -801,7 +801,7 @@ app.post('/admin/review-reschedule/:id', checkAuthenticated, checkAdmin, (req, r
 });
 // En Hui's Part End.
 
-// Starting the server
+// Starting the server - Rach
 app.listen(3000, () => {
     console.log('Server started on port 3000');
 });
